c1b909ca47b625add77cda077ce221857e18248e
mlefloch12 <mathys.lefloch12@gmail.com>
2023-05-06 20:27:50 +0200

    Continuité de la page Home

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -2,10 +2,10 @@ import { FaTiktok, FaInstagram } from 'react-icons/fa'
 
 export const SocialLinks = () => {
     return (
-        <section className="mt-5">
-            <h3 className="text-2xl font-extrabold">Mes réseaux sociaux</h3>
+        <section className="mt-20">
+            <h3 className="text-3xl font-extrabold">Mes réseaux sociaux</h3>
 
-            <ul className="mt-5">
+            <ul className="text-2xl mt-5 mb-5">
                 <li className='flex items-center'><FaTiktok className='mr-1'/><a className="hover:font-bold" href="https://www.tiktok.com/@mist.off">TikTok</a></li>
                 <li className='flex items-center'><FaInstagram className='mr-1'/><a className="hover:font-bold" href="https://www.instagram.com/mist.off/">Instagram</a></li>
             </ul>
